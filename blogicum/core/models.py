4b61fb150818eecae47489128478571258771116
Григорий Рогов <gregory.rogoff@yandex.ru>
2023-05-17 00:03:43 +0400

    first push

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -9,6 +9,6 @@ class BaseModel(models.Model):
         help_text='Снимите галочку,чтобы скрыть публикацию.')
     created_at = models.DateTimeField(
         auto_now_add=True, null=False, verbose_name='Добавлено')
-    
+
     class Meta:
         abstract = True
