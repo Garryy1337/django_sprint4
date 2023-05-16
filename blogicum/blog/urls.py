33ef6aae87c2cd29fe2a2c8fc1c804ced6402629
Григорий Рогов <gregory.rogoff@yandex.ru>
2023-05-17 00:18:34 +0400

    first push

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -13,24 +13,23 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', login_required(
      views.PostUpdateView.as_view()),
          name='edit_post'),
-    path('posts/<int:pk>/delete/', login_required(
-     views.PostDeleteView.as_view()),
+    path('posts/<int:pk>/delete/',
+         login_required(views.PostDeleteView.as_view()),
          name='delete_post'),
-    path('category/<slug:category_slug>/', login_required(
-     views.CategoryListView.as_view()),
+    path('category/<slug:category_slug>/',
+         login_required(views.CategoryListView.as_view()),
          name='category_posts'),
     path('profile/<slug:username>/', views.ProfileListView.as_view(),
          name='profile'),
-    path('edit_profile/', login_required(
-     views.ProfileUpdateView.as_view()),
+    path('edit_profile/', login_required(views.ProfileUpdateView.as_view()),
          name='edit_profile'),
-    path('posts/<int:pk>/comment/', login_required(
-     views.CommentAddView.as_view()),
+    path('posts/<int:pk>/comment/',
+         login_required(views.CommentAddView.as_view()),
          name='add_comment'),
-    path('posts/<int:pk>/edit_comment/<int:id>/', login_required(
-     views.CommentUpdateView.as_view()),
+    path('posts/<int:pk>/edit_comment/<int:id>/',
+         login_required(views.CommentUpdateView.as_view()),
          name='edit_comment'),
-    path('posts/<int:pk>/delete_comment/<int:id>/', login_required(
-     views.CommentDeleteView.as_view()),
+    path('posts/<int:pk>/delete_comment/<int:id>/',
+         login_required(views.CommentDeleteView.as_view()),
          name='delete_comment'),
 ]
